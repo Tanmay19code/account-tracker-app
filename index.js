2c253c584c2f1773a1ba48ec5387bc4beaf3506c
Tanmay19code <tanmaymutalik2002@gmail.com>
2024-07-21 01:19:58 +0530

    add build files and vercel configuration

--- index.js
+++ index.js
@@ -13,13 +13,18 @@ connectToMongo();
 app.use(morgan("dev"));
 app.use(express.json());
 
+app.use(express.static("views/client/build"));
+
 app.use("/api/auth", require("./routes/auth.route"));
 app.use("/api/account", require("./routes/account.route"));
 app.use("/api/category", require("./routes/category.route"));
 app.use("/api/transaction", require("./routes/transaction.route"));
 app.use("/api/saving", require("./routes/saving.route"));
 
-
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve('views', 'client', 'build', 'index.html'));
+});
+ 
 
 app.listen(5000, () => {
   console.log(`Server is running on port ${PORT}`.yellow.italic.underline.bold);
